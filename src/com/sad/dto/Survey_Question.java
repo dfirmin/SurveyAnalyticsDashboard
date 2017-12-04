a97b226a79e1178fb1d058976cdcf23b32e98d69
Alex Cyr <alexcyr@Alex-Cyrs-MacBook-Pro.local>
2017-12-04 16:34:48 -0500

    configure dto classes

--- src/com/sad/dto/Survey_Question.java
+++ src/com/sad/dto/Survey_Question.java
@@ -1,5 +1,30 @@
 package com.sad.dto;
 
-public class Survey_Question {
+import java.io.Serializable;
 
+public class Survey_Question implements Serializable{
+
+	private int surveyID;
+	private int questionID;
+	
+	public Survey_Question() {
+		
+	}
+
+	public int getSurveyID() {
+		return surveyID;
+	}
+
+	public void setSurveyID(int surveyID) {
+		this.surveyID = surveyID;
+	}
+
+	public int getQuestionID() {
+		return questionID;
+	}
+
+	public void setQuestionID(int questionID) {
+		this.questionID = questionID;
+	}
+	
 }
--- src/com/sad/dto/Users.java
+++ src/com/sad/dto/Users.java
@@ -1,5 +1,65 @@
 package com.sad.dto;
 
-public class Users {
+import java.io.Serializable;
 
+public class Users implements Serializable{
+	private String firstName;
+	private String LastName;
+	private String Email;
+	private String Password;
+	private int UserID;
+	private int PersonID;
+	
+	public Users() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public int getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+
+	public int getPersonID() {
+		return PersonID;
+	}
+
+	public void setPersonID(int personID) {
+		PersonID = personID;
+	}
+	
 }
